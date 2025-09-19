21d57b660a7d00afad6ce81cdc14b25efb52d7a7
bantisingh20 <124371011+bantisingh20@users.noreply.github.com>
2025-09-19 09:10:29 +0700

    snackbar setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/authcontext.jsx";
+import { SnackbarProvider } from './components/SnackbarProvider'
+
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </AuthProvider>
   </StrictMode>
 );
